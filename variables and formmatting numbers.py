e24e50375974705c73852afd13358f71feec621c
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-01-30 11:18:43 -0600

    Add files via upload

---
+++ variables and formmatting numbers.py
@@ -0,0 +1,14 @@
+my_variable = 42
+
+my_new_variable = my_variable * 2 
+
+print(my_variable)
+print(my_new_variable)
+
+my_variable = 7
+my_new_variable = my_variable
+
+kilograms = 10
+conversion = 2.20462
+pounds = kilograms * conversion 
+print(pounds)
