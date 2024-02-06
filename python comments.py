3f4e6f467435d6742587ffb5e9b0b91e113f59d6
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-02-05 22:43:54 -0600

    Add files via upload

---
+++ python comments.py
@@ -0,0 +1,8 @@
+tank_size = 15
+#this is how many gallons are in your car
+distance = 600
+#over all distance before you run out of gas
+mpg = distance / tank_size
+#divide 600/15 to get the mpg
+print("Miles per gallon:", mpg)
+#the mpg should be 40 miles per gallon
